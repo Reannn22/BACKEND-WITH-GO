2f56d2c0f4034a66ab8c88299eb5b58c0f52e657
Reyhan Capri Moraga <reyhan.123140022@student.itera.ac.id>
2024-11-11 22:54:28 +0700

    Add files via upload

---
+++ BACKEND/variabel/variabel.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "my"
+	const lastName = "skill"
+
+	firstName = "aku"
+	
+	// var bilanganBulat uint8 = 20
+
+	//var bilanganDesimal = 2.0
+
+	//var varBool = true
+
+	var numberA int = 4
+	var numberB	*int = &numberA
+	*numberB = 8
+
+	fmt.Println("halo ", firstName, lastName, numberA, "!\n")
+
+}
