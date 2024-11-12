8bc25d3bbce6690357fab4b28cebabc81b3417e9
Reyhan Capri Moraga <reyhan.123140022@student.itera.ac.id>
2024-11-12 23:33:17 +0700

    Delete BACKEND directory

--- BACKEND/casting/casting.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var a float64 = float64(24)
-	fmt.Println(a) // 24
-
-}
--- BACKEND/inference/inference.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var firstName string = "my"
-	lastName := 10
-	fmt.Printf("halo %s %s!\n", firstName, lastName)
-}
